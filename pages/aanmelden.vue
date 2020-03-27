815a89dbc3a09a18c1b36c6de8f8e58a6e6495d0
joostlek <joostlek@outlook.com>
2020-03-27 11:16:01 +0100

    Apply ESLint

--- pages/aanmelden.vue
+++ pages/aanmelden.vue
@@ -9,12 +9,12 @@
       </TextLink>
       .
     </p>
-    <input v-model="registration.firstName" type="text" placeholder="Voornaam"/><br/>
-    <input v-model="registration.middleName" type="text" placeholder="Tussevoegsel"/><br/>
-    <input v-model="registration.lastName" type="text" placeholder="Achternaam"/><br/>
-    <input v-model="registration.mailAddress" type="text" placeholder="Emailadres"/><br/>
-    <input v-model="registration.phoneNumber" type="text" placeholder="Telefoonnummer"/><br/>
-    <input v-model="registration.dateOfBirth" type="date" placeholder="Geboortedatum"/><br/>
+    <input v-model="registration.firstName" type="text" placeholder="Voornaam" /><br />
+    <input v-model="registration.middleName" type="text" placeholder="Tussevoegsel" /><br />
+    <input v-model="registration.lastName" type="text" placeholder="Achternaam" /><br />
+    <input v-model="registration.mailAddress" type="text" placeholder="Emailadres" /><br />
+    <input v-model="registration.phoneNumber" type="text" placeholder="Telefoonnummer" /><br />
+    <input v-model="registration.dateOfBirth" type="date" placeholder="Geboortedatum" /><br />
     <select v-model="registration.studyTypeId">
       <option disabled :value="0">
         Selecteer je studierichting
@@ -22,13 +22,13 @@
       <option v-for="(studyType, idx) in studyTypes" :key="idx" :value="studyType.id">
         {{ studyType.name }}
       </option>
-    </select><br/>
-    <input id="statuten" v-model="registration.statuten" type="checkbox"/> <label for="statuten">Ik ga akkoord met de
-    <TextLink url="statuten" inline>statuten</TextLink>
-    van Studievereniging Indicium.</label><br/>
-    <input id="nieuwsbrief" v-model="registration.toReceiveNewsletter" type="checkbox"/> <label for="nieuwsbrief">Ik wil
-    graag de maandelijkse nieuwsbrief ontvangen met aankomende activiteiten, nieuwtjes en updates van de
-    vereniging.</label><br/>
+    </select><br />
+    <input id="statuten" v-model="registration.statuten" type="checkbox" /> <label for="statuten">Ik ga akkoord met de
+      <TextLink url="statuten" inline>statuten</TextLink>
+      van Studievereniging Indicium.</label><br />
+    <input id="nieuwsbrief" v-model="registration.toReceiveNewsletter" type="checkbox" /> <label for="nieuwsbrief">Ik wil
+      graag de maandelijkse nieuwsbrief ontvangen met aankomende activiteiten, nieuwtjes en updates van de
+      vereniging.</label><br />
     <Button @click.native="saveRegistration">
       Meld je aan
     </Button>
@@ -41,49 +41,49 @@
   import Button from '../components/interactions/button'
 
   export default {
-    name: 'Aanmelden',
-    components: {
-      Button,
-      TextLink
+  name: 'Aanmelden',
+  components: {
+    Button,
+    TextLink
+  },
+  data() {
+    return {
+      registration: {
+        firstName: null,
+        middleName: null,
+        lastName: null,
+        mailAddress: null,
+        phoneNumber: null,
+        dateOfBirth: new Date(),
+        studyTypeId: 0,
+        statuten: false,
+        toReceiveNewsletter: false
+      },
+      studyTypes: [],
+      loading: false
+    }
+  },
+  mounted() {
+    this.getStudyTypes()
+  },
+  methods: {
+    getStudyTypes() {
+      this.$api.get('/studytype')
+        .then((res) => {
+          this.studyTypes = res.data
+        })
     },
-    data() {
-      return {
-        registration: {
-          firstName: null,
-          middleName: null,
-          lastName: null,
-          mailAddress: null,
-          phoneNumber: null,
-          dateOfBirth: new Date(),
-          studyTypeId: 0,
-          statuten: false,
-          toReceiveNewsletter: false
-        },
-        studyTypes: [],
-        loading: false
+    async saveRegistration() {
+      this.loading = true
+      const res = await this.$api.post('/registration', this.registration)
+      if (res.status === 201) {
+        this.loading = false
       }
     },
-    mounted() {
-      this.getStudyTypes()
-    },
-    methods: {
-      getStudyTypes() {
-        this.$api.get('/studytype')
-          .then((res) => {
-            this.studyTypes = res.data
-          })
-      },
-      async saveRegistration() {
-        this.loading = true
-        const res = await this.$api.post('/registration', this.registration)
-        if (res.status === 201) {
-          this.loading = false
-        }
-      },
-      validateRegistration() {
-      }
+    validateRegistration() {
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
