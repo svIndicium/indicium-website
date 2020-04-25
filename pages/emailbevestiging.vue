1e31c96105d7957269f7682d28695c3bee1cd04e
joostlek <joostlek@outlook.com>
2020-04-26 01:59:25 +0200

    Finish sign up flow

---
+++ pages/emailbevestiging.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="container">
+    <h2>Emailadres Bevestigen</h2>
+    <div v-if="error">
+      <div class="errorcontainer">
+        <Icon type="alert-circle" class="icon" />
+        <span class="message">
+          {{ error }}
+        </span>
+      </div>
+    </div>
+    <div v-if="result !== null">
+      {{ result }}
+    </div>
+    <div v-else-if="loading === true">
+      <Loading />
+    </div>
+    <div v-else>
+      Om je email te bevestigen heb je een code in je mailbox ontvangen (check ook je spam). Heb je geen mail ontvangen?
+      Neem dan even contact op met secretaris@indicium.hu.<br /><br />
+      <TextInput v-model="verification.address" label="Emailadres" placeholder="Emailadres" /><br />
+      <TextInput v-model="verification.token" label="Token" placeholder="Bevestigingstoken" /><br />
+      <Button size="l" @click.native="verifyMail">
+        Bevestig
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Button from '../components/interactions/button'
+  import Loading from '../components/Loading'
+  import TextInput from '../components/interactions/TextInput'
+  import Icon from '../components/Icon'
+
+  export default {
+  name: 'Emailverificatie',
+  components: {
+    TextInput,
+    Button,
+    Loading,
+    Icon,
+  },
+  data() {
+    return {
+      verification: {},
+      loading: false,
+      manual: false,
+      result: null,
+      error: null
+    }
+  },
+  async mounted() {
+    this.loading = true
+    await this.getParameters()
+  },
+  methods: {
+    async getParameters() {
+      const email = this.$route.query.email || null
+      const token = this.$route.query.token || null
+      if (email === null || token === null) {
+        this.manual = true
+        this.loading = false
+      } else {
+        this.verification = {
+          address: email,
+          token
+        }
+        await this.verifyMail()
+      }
+    },
+    async verifyMail() {
+      this.loading = true
+      try {
+        await this.$api.post('/mail/verify', this.verification)
+        this.$router.push('/emailbevestigd')
+      } catch (e) {
+        this.error = e
+      }
+      this.loading = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .errorcontainer {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    color: var(--indi-error);
+    margin-bottom: 16px;
+
+    .icon {
+      width: 32px;
+      height: 32px;
+      font-size: 32px;
+    }
+
+    .message {
+      padding-left: 8px;
+    }
+  }
+</style>
