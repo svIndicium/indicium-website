954f2892e3b946124a6ef4ed1580aafec4aa0f91
joostlek <joostlek@outlook.com>
2020-08-10 15:49:56 +0200

    Update mode configuration

--- plugins/axios.js
+++ plugins/axios.js
@@ -5,11 +5,11 @@ import DataMiddleware from '../middleware/api'
 
 const api = {}
 api.install = (Vue) => {
-  const baseURL = process.env.NUXT_ENV_APP_BRANCH === 'dev'
-    ? 'http://localhost:8080/api/v1'
-    : process.env.NODE_ENV === 'production' && process.env.NUXT_ENV_APP_BRANCH === 'master'
-      ? 'https://lit.indicium.hu/api/v1'
-      : 'https://lit.dev.indicium.hu/api/v1'
+  const baseURL = process.env.NUXT_ENV_MODE === 'live'
+    ? 'https://api.indicium.hu/api/v1'
+    : process.env.NUXT_ENV_MODE === 'dev'
+      ? 'https://dev.api.indicium.hu/api/v1'
+      : 'https://localhost:8080/api/v1'
 
   const apiInstance = axios.create({ baseURL })
 
