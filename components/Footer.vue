d13f0c34e0695c7e31190b66063e9f8afe41e848
Olivier Verwoerd <olivier.verwoerd@student.hu.nl>
2021-02-25 13:04:23 +0100

    Header and footer border color

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,34 +1,34 @@
 <template>
-    <footer>
-        <div class="container">
-            <div class="column">
-                <h4>Pagina's</h4>
-                <ul class="footer-menu pages">
-                    <li v-for="(menuItem, idx) in menuItems" :key="idx">
-                        <a :href="menuItem.link">{{ menuItem.title }}</a>
-                    </li>
-                </ul>
-                <div class="toggle contrast">
-                    <input
-                        id="toggle-contrast"
-                        type="checkbox"
-                        @change="changeContrast"
-                        :checked="isContrasted"
-                    />
-                    <label for="toggle-contrast">Hoger contrast</label>
-                </div>
-                <div class="toggle dark-mode">
-                    <input
-                        id="dark-mode"
-                        type="checkbox"
-                        @change="changeDarkMode"
-                        :checked="isDark"
-                    />
-                    <label for="dark-mode">Dark Mode</label>
-                </div>
-            </div>
+  <footer>
+    <div class="container">
+      <div class="column">
+        <h4>Pagina's</h4>
+        <ul class="footer-menu pages">
+          <li v-for="(menuItem, idx) in menuItems" :key="idx">
+            <a :href="menuItem.link">{{ menuItem.title }}</a>
+          </li>
+        </ul>
+        <div class="toggle contrast">
+          <input
+            id="toggle-contrast"
+            type="checkbox"
+            @change="changeContrast"
+            :checked="isContrasted"
+          />
+          <label for="toggle-contrast">Hoger contrast</label>
+        </div>
+        <div class="toggle dark-mode">
+          <input
+            id="dark-mode"
+            type="checkbox"
+            @change="changeDarkMode"
+            :checked="isDark"
+          />
+          <label for="dark-mode">Dark Mode</label>
+        </div>
+      </div>
 
-            <div class="column">
+      <div class="column">
         <h4>Social media</h4>
         <ul class="footer-menu contact">
           <li v-for="(menuItem, key) in socialMediaItems" :key="key">
@@ -38,62 +38,62 @@
       </div>
 
       <div class="column">
-                <h4>Contact</h4>
-                <ul class="footer-menu contact">
-                    <li v-for="(value, key) in contactInfo" :key="key">{{ value }}</li>
-                </ul>
-            </div>
-        </div>
-    </footer>
+        <h4>Contact</h4>
+        <ul class="footer-menu contact">
+          <li v-for="(value, key) in contactInfo" :key="key">{{ value }}</li>
+        </ul>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        isDark: false,
-        isContrasted: false,
-        contactInfo: {
-            address: "Heidelberglaan 15",
-            postcode: "3584 CS Utrecht",
-            lokaal: "HL15-4.092",
-            email: "bestuur@indicium.hu",
-        },
-        menuItems: [
-            {
-                title: "Inschrijven",
-                link:
-                    "https://docs.google.com/forms/d/e/1FAIpQLSeYE3T-IdYrxb1FB2wKGtltf5D16aLzsDdaW3g1IZAj5Lxj3Q/viewform",
-            },
-            // {
-            //   title: 'Vacatures',
-            //   link: '#'
-            // },
-            {
-                title: "Statuten",
-                link: "/statuten",
-            },
-            {
-                title: "Over Indicium",
-                link: "/over-indicium",
-            },
-            // {
-            //   title: 'Uitleenservice',
-            //   link: '#'
-            // },
-            {
-                title: "Activiteiten",
-                link: "/activiteiten",
-            },
-            {
-                title: "Sponsoren en partners",
-                link: "/partners",
-            },
-            {
-                title: "Privacyreglement",
-                link: "/privacyreglement",
-            },
-        ],
-    socialMediaItems: [
+  data: () => ({
+    isDark: false,
+    isContrasted: false,
+    contactInfo: {
+      address: "Heidelberglaan 15",
+      postcode: "3584 CS Utrecht",
+      lokaal: "HL15-4.092",
+      email: "bestuur@indicium.hu",
+    },
+    menuItems: [
+      {
+        title: "Inschrijven",
+        link:
+          "https://docs.google.com/forms/d/e/1FAIpQLSeYE3T-IdYrxb1FB2wKGtltf5D16aLzsDdaW3g1IZAj5Lxj3Q/viewform",
+      },
+      // {
+      //   title: 'Vacatures',
+      //   link: '#'
+      // },
+      {
+        title: "Statuten",
+        link: "/statuten",
+      },
+      {
+        title: "Over Indicium",
+        link: "/over-indicium",
+      },
+      // {
+      //   title: 'Uitleenservice',
+      //   link: '#'
+      // },
+      {
+        title: "Activiteiten",
+        link: "/activiteiten",
+      },
+      {
+        title: "Sponsoren en partners",
+        link: "/partners",
+      },
+      {
+        title: "Privacyreglement",
+        link: "/privacyreglement",
+      },
+    ],
+  socialMediaItems: [
       {
         title: 'Facebook',
         link: 'https://www.facebook.com/SVIndicium'
@@ -111,57 +111,57 @@ export default {
         link: 'https://www.linkedin.com/company/s.v.indicium'
       }
     ]}),
-    methods: {
-        changeContrast(e) {
-            const isChecked = e.target.checked;
-            this.$set(this, "isContrasted", isChecked);
-            if (!this.isDark) {
-                document.documentElement.style.setProperty(
-                    "--text-color",
-                    this.isContrasted ? "#000" : "#616364"
-                );
-            }
-        },
-        changeDarkMode(e) {
-            this.$set(this, "isDark", e.target.checked);
-            this.$eventBus.$emit("dark-mode", this.isDark);
-            window.localStorage.setItem("IS_DARK", this.isDark);
-            if (this.isDark) {
-                document.documentElement.setAttribute("data-theme", "dark");
-                document.documentElement.style.setProperty(
-                    "--text-color",
-                    "#fff"
-                ); // ugly guard...
-            } else {
-                document.documentElement.setAttribute("data-theme", "light");
-                document.documentElement.style.setProperty(
-                    "--text-color",
-                    this.isContrasted ? "#000" : "#616364"
-                ); // ugly guard...
-            }
-        },
-        saveDarkMode() {
-            const isDarkPersisted = window.localStorage.getItem("IS_DARK");
-            const isDark = isDarkPersisted === "true";
-            const doesntWantDark = isDarkPersisted === "false";
-            const isNativeDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
-            const shouldBeDarkOrNot = (isNativeDark || isDark) && !doesntWantDark;
-            if (shouldBeDarkOrNot) {
-                this.$set(this, "isDark", shouldBeDarkOrNot);
-                this.changeDarkMode({ target: { checked: this.isDark } }); // h4c
-            }
-        },
+  methods: {
+    changeContrast(e) {
+      const isChecked = e.target.checked;
+      this.$set(this, "isContrasted", isChecked);
+      if (!this.isDark) {
+        document.documentElement.style.setProperty(
+          "--text-color",
+          this.isContrasted ? "#000" : "#616364"
+        );
+      }
     },
-    watch: {
-        $route(to, from) { // h4c: correct logo render on page change
-          console.log('hehe')
+    changeDarkMode(e) {
+      this.$set(this, "isDark", e.target.checked);
+      this.$eventBus.$emit("dark-mode", this.isDark);
+      window.localStorage.setItem("IS_DARK", this.isDark);
+      if (this.isDark) {
+        document.documentElement.setAttribute("data-theme", "dark");
+        document.documentElement.style.setProperty("--text-color", "#fff"); // ugly guard...
+      } else {
+        document.documentElement.setAttribute("data-theme", "light");
+        document.documentElement.style.setProperty(
+          "--text-color",
+          this.isContrasted ? "#000" : "#616364"
+        ); // ugly guard...
+      }
+    },
+    saveDarkMode() {
+      const isDarkPersisted = window.localStorage.getItem("IS_DARK");
+      const isDark = isDarkPersisted === "true";
+      const doesntWantDark = isDarkPersisted === "false";
+      const isNativeDark =
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches;
+      const shouldBeDarkOrNot = (isNativeDark || isDark) && !doesntWantDark;
+      if (shouldBeDarkOrNot) {
+        this.$set(this, "isDark", shouldBeDarkOrNot);
+        this.changeDarkMode({ target: { checked: this.isDark } }); // h4c
+      }
+    },
+  },
+  watch: {
+    $route(to, from) {
+      // h4c: correct logo render on page change
+      console.log("hehe");
 
-          setTimeout(this.saveDarkMode)
-        },
+      setTimeout(this.saveDarkMode);
     },
-    mounted() {
-      this.saveDarkMode();
-    },
+  },
+  mounted() {
+    this.saveDarkMode();
+  },
 };
 </script>
 
@@ -169,35 +169,36 @@ export default {
 @import "../assets/scss/variables.scss";
 
 footer {
-    background: var(--indi-blue-3);
-    padding: 80px;
-    color: var(--text-color);
-    margin-top: 32px;
+  background: var(--indi-green-3);
+  padding: 80px;
+  color: var(--text-color);
+  margin-top: 16px;
+  border-top: 2px solid var(--indi-green-1);
+
+  @media screen and (max-width: $bp-tablet-sm) {
+    padding: 12px;
+  }
+
+  .container {
+    max-width: 1084px;
+    margin: 0 auto;
+    padding: 0 24px;
+    display: flex;
+    justify-content: space-between;
 
     @media screen and (max-width: $bp-tablet-sm) {
-        padding: 12px;
+      display: block;
     }
 
-    .container {
-        max-width: 1084px;
-        margin: 0 auto;
-        padding: 0 24px;
-        display: flex;
-        justify-content: space-between;
+    .footer-menu {
+      &.pages {
+        columns: 2;
+      }
 
-        @media screen and (max-width: $bp-tablet-sm) {
-            display: block;
-        }
-
-        .footer-menu {
-            &.pages {
-                columns: 2;
-            }
-
-            li {
-                line-height: 1.6;
-            }
-        }
+      li {
+        line-height: 1.6;
+      }
     }
+  }
 }
 </style>
