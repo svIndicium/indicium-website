4619f4fc16280ce131ba1c4495c0681ba4610729
Olivier Verwoerd <olivier.verwoerd@student.hu.nl>
2021-04-07 20:33:45 +0200

    fix menu left if prefered

--- components/Nav.vue
+++ components/Nav.vue
@@ -32,6 +32,9 @@
           <n-link to="/" prefetch @click.native="hideNav">Home</n-link>
         </li>
         <li v-for="item in items" :key="item.title + item.url + item.childs">
+          <script>
+            console.log(items);
+          </script>
           <a
             v-if="item.url.startsWith('http')"
             :href="item.url"
@@ -59,8 +62,15 @@
             <li
               class="sub-menu-li"
               v-for="child in item.childs"
-              :key="child.title + child.url + child.childs"
+              :key="child.title + child.url + child.childs + child.childs_side"
             >
+              <label
+                v-if="child.childs && child.childs_left"
+                title="Toggle Drop-down"
+                class="drop-icon"
+                >◂</label
+              >
+
               <a
                 v-if="child.url.startsWith('http')"
                 :href="child.url"
@@ -75,13 +85,39 @@
               </n-link>
 
               <label
-                v-if="child.childs"
+                v-if="child.childs && !child.childs_left"
                 title="Toggle Drop-down"
                 class="drop-icon"
                 >▸</label
               >
 
-              <ul class="sub-sub-menu">
+              <ul v-if="!child.childs_left" class="sub-sub-menu">
+                <li
+                  class="sub-sub-menu-li"
+                  v-for="grand_child in child.childs"
+                  :key="grand_child.title + grand_child.url"
+                >
+                  <a
+                    v-if="grand_child.url.startsWith('http')"
+                    :href="grand_child.url"
+                    target="_blank"
+                    @click="hideNav"
+                  >
+                    {{ grand_child.title }}
+                  </a>
+
+                  <n-link
+                    v-else
+                    :to="item.url"
+                    prefetch
+                    @click.native="hideNav"
+                  >
+                    {{ grand_child.title }}
+                  </n-link>
+                </li>
+              </ul>
+
+              <ul v-if="child.childs_left" class="sub-sub-menu-left">
                 <li
                   class="sub-sub-menu-li"
                   v-for="grand_child in child.childs"
@@ -226,6 +262,7 @@ export default {
                 url: "/over-indicium",
               },
             ],
+            childs_left: true,
           },
           {
             title: "Welke koffie moet ik halen?",
@@ -380,6 +417,20 @@ export default {
             }
           }
         }
+        .sub-sub-menu-left {
+          opacity: 1;
+          visibility: visible;
+          width: 200px;
+
+          .sub-sub-menu-li {
+            width: 100%;
+            height: auto;
+            .a {
+              text-decoration: none;
+              width: 100%;
+            }
+          }
+        }
       }
     }
 
@@ -389,6 +440,29 @@ export default {
       flex-direction: column;
       background: var(--root-background-color);
       left: 100%;
+      margin-top: 48px;
+      /* // todo put it better nex to eachther*/
+      box-shadow: inset 0 0 0 2px var(--indi-green-1);
+      opacity: 1;
+      visibility: hidden; /*hidden   inset 0 -2px 0 var(--indi-blue-1);*/
+      z-index: 2;
+
+      a {
+        padding-left: 15px;
+        padding-right: 15px;
+      }
+
+      li {
+        width: 200px;
+      }
+    }
+
+    .sub-sub-menu-left {
+      /*display: none;*/
+      position: absolute;
+      flex-direction: column;
+      background: var(--root-background-color);
+      left: -100%;
       margin-top: 48px;
       /* // todo put it better nex to eachther*/
       box-shadow: inset 0 0 0 2px var(--indi-green-1);
